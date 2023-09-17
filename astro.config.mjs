3c0942760a7a10c60c338721af89013078d16476
antocharli <anto.a@infosysequinox.com>
2023-09-17 02:43:53 -0600

    Updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   // Enable React to support React JSX components.
   integrations: [react()],
-  output: 'server',
-  adapter: node({
-    mode: "standalone"
-  })
+  output: 'server'
+  // adapter: node({
+  //   mode: "standalone"
+  // })
+  ,
+  adapter: cloudflare({ mode: "directory" })
 });
