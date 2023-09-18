1b644aef10eb862a7d5e2428be660a623029de5d
antocharli <anto.a@infosysequinox.com>
2023-09-18 09:29:27 -0600

    trying API

--- src/api/hello.ts
+++ src/api/hello.ts
@@ -1,9 +1,9 @@
 import type { APIRoute } from "astro";
 
-export const GET: APIRoute = () => {
-  return new Response(
-    JSON.stringify({
-      greeting: "Hello",
-    })
+export const GET: APIRoute = async () => {
+  const catalogResponse = await fetch(
+    "https://fakestoreapi.com/products?limit=10"
   );
+  const catalogData = await catalogResponse.json();
+  return new Response(JSON.stringify(catalogData));
 };
